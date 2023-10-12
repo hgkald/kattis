166e701b4c536d870bb8b1d920c178c45ccc2261
hana <17885726+hgonzal@users.noreply.github.com>
2023-10-12 09:31:26 +0200

    Fix bug with parallel edges

--- minspantree/minspantree.py
+++ minspantree/minspantree.py
@@ -1,13 +1,16 @@
 import sys 
+import queue
 import random 
 from functools import total_ordering 
+from collections import defaultdict 
 import heapq
 #import cProfile 
 
 class Graph:
     def __init__(self):
-        self.nodes = []
-        self.edges = {}
+        self.nodes = {}
+        self.edges = defaultdict(list)
+        self.num_edges = 0
 
     def node(self, val):
         return self.nodes[int(val)]
@@ -17,7 +20,8 @@ class Graph:
 
     def add_edge(self, edge):
         key = frozenset((edge.node1.val, edge.node2.val))
-        self.edges[key] = edge
+        self.edges[key].append(edge)
+        self.num_edges += 1
 
 class Node:
     def __init__(self, val):
@@ -42,6 +46,22 @@ class Edge:
         else:
             return None
 
+@total_ordering
+class ComparableItem: 
+    def __init__(self, priority, item): 
+        self.item = item 
+        self.priority = priority
+
+    def __lt__(self, other):
+        return self.priority < other.priority
+
+    def __gt__(self, other):
+        return self.priority > other.priority
+
+    def __eq__(self, other):
+        return self.priority == other.priority
+
+
 class MyQueue:
     def __init__(self):
         self.items = []
@@ -58,24 +78,21 @@ class MyQueue:
 
 
 def prim(g): 
+    #q = queue.PriorityQueue()
     q = MyQueue()
     parents = {}
 
-    some_node = random.choice(g.nodes)
-    q.put((0, None, some_node.val))
+    some_node = random.choice(list(g.nodes.values()))
+    q.put(ComparableItem(0, (None, some_node)))
 
     while not q.empty(): 
-        (weight, parentid, nodeid) = q.get()
-        parent = None 
-        if parentid: 
-            parent = g.nodes[parentid]
-        node = g.nodes[nodeid]
+        (parent, node) = q.get().item
         if node not in parents:
             parents[node] = parent
             for edge in node.edges: 
                 child = edge.other_node(node)
-                if child not in parents: 
-                    q.put((edge.weight, node.val, child.val))
+                #if child not in parents: 
+                q.put(ComparableItem(edge.weight, (node, child)))
 
     return parents
 
@@ -85,19 +102,17 @@ def __main__():
         g = Graph()
         n = int(vals[0])
         m = int(vals[1])
-        g.nodes = [None] * n
-
         for i in range(int(vals[1])): 
             [n1, n2, w] = [int(val) for val in input().split()]
-            if not g.nodes[n1]: 
+            if n1 not in g.nodes.keys(): 
                 node1 = Node(n1)
-                g.nodes[n1] = node1
+                g.add_node(node1)
             else: 
                 node1 = g.nodes[n1]
 
-            if not g.nodes[n2]: 
+            if n2 not in g.nodes.keys(): 
                 node2 = Node(n2)
-                g.nodes[n2] = node2
+                g.add_node(node2)
             else: 
                 node2 = g.nodes[n2]
 
@@ -106,27 +121,27 @@ def __main__():
             node1.add_edge(edge)
             node2.add_edge(edge)
 
-        if len(g.edges) > 0: 
+        if g.num_edges > 0: 
             parents = prim(g)
             nodes_reached = len(parents)
             if nodes_reached < n: 
                 print("Impossible")
-                continue
+            else: 
+                edges = [] 
+                cost = 0
+                for node in parents: 
+                    if parents[node]:
+                        parent = parents[node]
+                        weights = [e.weight for e in g.edges[frozenset((parent.val, node.val))]]
+                        edge = [parent.val, node.val]
+                        cost += min(weights)
+                        edge.sort()
+                        edges.append(edge)
+                print(cost)
 
-            edges = [] 
-            cost = 0
-            for node in parents: 
-                if parents[node]:
-                    parent = parents[node]
-                    cost += g.edges[frozenset((parent.val, node.val))].weight
-                    edge = [parent.val, node.val]
-                    edge.sort()
-                    edges.append(edge)
-            print(cost)
-
-            edges.sort()
-            for e in edges: 
-                print(e[0], e[1])
+                edges.sort()
+                for e in edges: 
+                    print(e[0], e[1])
         else: 
             print("Impossible")
         vals = input().split()
